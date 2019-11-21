55e54d0217a461c993794e45a43a8cf6f701ec4a
Ricardo Luna <r_luna99@hotmail.com>
2019-11-21 15:44:31 -0700

    Adding the sign_up page

--- src/main.py
+++ src/main.py
@@ -4,5 +4,9 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('main_page.html')
+
+@app.route('/signup')
+def sign_up():
+	return render_template('sign_up.html')
