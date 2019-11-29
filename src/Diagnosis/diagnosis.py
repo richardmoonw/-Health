59663a2723390591d9e894c3ebca79fb24deed3b
Ricardo Luna <r_luna99@hotmail.com>
2019-11-28 21:20:32 -0700

    New Diagnosis Working

--- src/Diagnosis/diagnosis.py
+++ src/Diagnosis/diagnosis.py
@@ -1,7 +1,7 @@
 class Diagnosis:
 	def __init__(self, date_created, description, doctor_id):
+		self.patient_id = 1
+		self.type = "diagnosis"
 		self.date_created = date_created
 		self.description = description
 		self.doctor_id = doctor_id
-
-		self.type = "diagnosis"
