11313557f9c46b7a422ca00c485d390c39443705
Cindy7td <chocolatectd@gmail.com>
2019-11-28 19:49:29 -0700

    diagnosis

--- src/Diagnosis/diagnosis_dao.py
+++ src/Diagnosis/diagnosis_dao.py
@@ -2,16 +2,38 @@ from DatabaseConnection import connection
 
 class DiagnosisDAO:
     def add_diagnosis(diagnosis):
-		conn = connection.Connection.make_connection()
+        conn = connection.Connection.make_connection()
 
-		cur = conn.cursor()
+        cur = conn.cursor()
 
         cur.execute("SELECT id FROM Doctor WHERE name='" + diagnosis.doctor_id + "'") 
-		doctor_id = cur.fetchone()
+        doctor_id = cur.fetchone()
 
         cur.execute("INSERT INTO Diagnosis (date_created, description, doctor_id) VALUES(?,?,?)", \
-					(diagnosis.date_created, diagnosis.description, diagnosis.doctor_id[0]))
+                    (diagnosis.date_created, diagnosis.description, diagnosis.doctor_id[0]))
 
         conn.commit()
 
-		conn.close()
+        conn.close()
+
+    def get_doctor_id(id):
+        conn = connection.Connection.make_connection()
+
+        cur = conn.cursor()
+
+        cur.execute("SELECT doctor_id FROM Diagnosis WHERE Diagnosis.doctor_id=" + str(id))
+
+        doctor_id = cur.fetchone()
+
+        return doctor_id
+
+    # def get_file_id(id):
+	# 	conn = connection.Connection.make_connection()
+
+	# 	cur = conn.cursor()
+
+	# 	cur.execute("SELECT file_id FROM Diagnosis WHERE file_id ='" + file_id + "'")
+
+	# 	file_id = cur.fetchone()
+
+	# 	return file_id """
